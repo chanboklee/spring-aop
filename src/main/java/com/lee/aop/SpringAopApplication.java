bed3ad956fdf26a0ed950b1cbcdf57f2804836f1
chanboklee <chanboklee@naver.com>
2023-03-12 13:39:56 +0900

    Fix : spring-aop - apply @Aspect (spring-aop)

--- src/main/java/com/lee/aop/SpringAopApplication.java
+++ src/main/java/com/lee/aop/SpringAopApplication.java
@@ -1,9 +1,12 @@
 package com.lee.aop;
 
+import com.lee.aop.order.aop.AspectV1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(AspectV1.class)
 public class SpringAopApplication {
 
     public static void main(String[] args) {
