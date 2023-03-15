147b39f744a9a23b692eb7455a9dfa1d9b6e3d84
chanboklee <chanboklee@naver.com>
2023-03-15 23:41:30 +0900

    Fix : spring-aop - add advice

--- src/main/java/com/lee/aop/SpringAopApplication.java
+++ src/main/java/com/lee/aop/SpringAopApplication.java
@@ -2,13 +2,15 @@ package com.lee.aop;
 
 import com.lee.aop.order.aop.AspectV1;
 import com.lee.aop.order.aop.AspectV2;
+import com.lee.aop.order.aop.AspectV3;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 // @Import(AspectV1.class)
-@Import(AspectV2.class)
+// @Import(AspectV2.class)
+@Import(AspectV3.class)
 public class SpringAopApplication {
 
     public static void main(String[] args) {
