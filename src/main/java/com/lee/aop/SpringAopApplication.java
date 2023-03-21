f6020c0fcc2901754e5378ad744d00082cbf7e7b
chanboklee <chanboklee@naver.com>
2023-03-21 21:06:25 +0900

    Fix : spring-aop-pointcut - @annotation

--- src/main/java/com/lee/aop/SpringAopApplication.java
+++ src/main/java/com/lee/aop/SpringAopApplication.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Import;
 // @Import(AspectV2.class)
 // @Import(AspectV3.class)
 // @Import(AspectV4Pointcut.class)
-@Import({AspectV5Order.LogAspect.class, AspectV5Order.TransactionAspect.class})
+// @Import({AspectV5Order.LogAspect.class, AspectV5Order.TransactionAspect.class})
 public class SpringAopApplication {
 
     public static void main(String[] args) {
---
+++ src/test/java/com/lee/aop/pointcut/AtAnnotaionTest.java
@@ -0,0 +1,37 @@
+package com.lee.aop.pointcut;
+
+import com.lee.aop.order.aop.member.MemberService;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Slf4j
+@Import(AtAnnotaionTest.AtAnnotationAspect.class)
+@SpringBootTest
+public class AtAnnotaionTest {
+
+    @Autowired
+    MemberService memberService;
+
+    @Test
+    void success(){
+        log.info("memberService Proxy -> {}", memberService.getClass());
+        memberService.hello("helloA");
+    }
+
+    @Slf4j
+    @Aspect
+    static class AtAnnotationAspect {
+
+        @Around("@annotation(com.lee.aop.order.aop.member.annotation.MethodAop)")
+        public Object doAtAnnotation(ProceedingJoinPoint joinPoint) throws Throwable {
+            log.info("[@annotation] -> {}", joinPoint.getSignature());
+            return joinPoint.proceed();
+        }
+    }
+}
