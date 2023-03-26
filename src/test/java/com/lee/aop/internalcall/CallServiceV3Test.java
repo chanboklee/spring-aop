9bf0b1e0bf963b9d3f021843545e9e8f6d729a4d
chanboklee <chanboklee@naver.com>
2023-03-27 02:48:55 +0900

    Fix : spring-aop - alternative structural change

---
+++ src/test/java/com/lee/aop/internalcall/CallServiceV3Test.java
@@ -0,0 +1,25 @@
+package com.lee.aop.internalcall;
+
+import com.lee.aop.order.aop.internalcall.CallServiceV2;
+import com.lee.aop.order.aop.internalcall.CallServiceV3;
+import com.lee.aop.order.aop.internalcall.aop.CallLogAspect;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Slf4j
+@Import(CallLogAspect.class)
+@SpringBootTest
+public class CallServiceV3Test {
+
+    @Autowired
+    CallServiceV3 callServiceV3;
+
+    @Test
+    void external(){
+        callServiceV3.external();
+    }
+
+}
